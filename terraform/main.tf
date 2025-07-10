c154732565b40931ad601c2a6c8f22c6ea0357ec
DhareshP <dharesh.a.p@gmail.com>
2025-07-10 23:53:35 +0530

    Update main.th file

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,44 +22,44 @@ resource "aws_iam_role" "ec2_role" {
 }
 
 
-resource "aws_iam_role_policy" "ec2_policy" {
-  name = "ec2-policy"
-  role = aws_iam_role.ec2_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect   = "Allow"
-      Action   = ["s3:PutObject", "s3:GetObject"]
-      Resource = "${aws_s3_bucket.logs_bucket.arn}/*"
-    }]
-  })
-}
-
 # resource "aws_iam_role_policy" "ec2_policy" {
 #   name = "ec2-policy"
 #   role = aws_iam_role.ec2_role.id
 #
 #   policy = jsonencode({
 #     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect   = "Allow"
-#         Action   = [
-#           "s3:CreateBucket",
-#           "s3:PutObject",
-#           "s3:GetObject",
-#           "s3:ListBucket",
-#           "s3:DeleteObject",
-#           "s3:DeleteBucket"
-#         ]
-#         Resource = [
-#           aws_s3_bucket.logs_bucket.arn,
-#           "${aws_s3_bucket.logs_bucket.arn}/*"
-#         ]
-#       }
-#     ]
+#     Statement = [{
+#       Effect   = "Allow"
+#       Action   = ["s3:PutObject", "s3:GetObject"]
+#       Resource = "${aws_s3_bucket.logs_bucket.arn}/*"
+#     }]
 #   })
 # }
+
+resource "aws_iam_role_policy" "ec2_policy" {
+  name = "ec2-policy"
+  role = aws_iam_role.ec2_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "s3:CreateBucket",
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:DeleteObject",
+          "s3:DeleteBucket"
+        ]
+        Resource = [
+          aws_s3_bucket.logs_bucket.arn,
+          "${aws_s3_bucket.logs_bucket.arn}/*"
+        ]
+      }
+    ]
+  })
+}
 
 
