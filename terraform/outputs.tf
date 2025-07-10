180f4cb9db30d639cc714d4f44114b99709a784c
DhareshP <dharesh.a.p@gmail.com>
2025-07-11 01:15:23 +0530

    output.tf updates

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
 output "instance_public_ip" {
   description = "Public IP of the EC2 instance"
-  value       = aws_instance.app_server.public_ip
+  value       = aws_instance.my_ec2_instance.public_ip
 }
 
 output "s3_bucket_name" {
